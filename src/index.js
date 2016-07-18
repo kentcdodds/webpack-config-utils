491f4048a2d99bd7a2da9affb686aef59fd4f6cb
Kent C. Dodds <kent@doddsfamily.us>
2016-07-18 17:16:40 -0600

    WIP: Still working on docs and things

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
+/**
+ * See documentation for the `here` method [here](https://www.npmjs.com/package/path-here)
+ */
 export {default as here} from 'path-here'
+/**
+ * See documentation for the `combineLoaders` method [here](https://www.npmjs.com/webpack-combine-loaders)
+ */
 export {default as combineLoaders} from 'webpack-combine-loaders'
+
 export * from './prop-if'
 export * from './remove-empty'
 export * from './get-if-utils'
